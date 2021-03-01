54f80b40d9414f931cea1bfcc3a5a5ba95231eb5
johnrevelator <johnrevelator>
2021-03-01 18:22:01 +0300

    aklimov/WISTORY-147/release

--- app/src/main/java/ru/vvdev/storiesdemo/MainActivity.kt
+++ app/src/main/java/ru/vvdev/storiesdemo/MainActivity.kt
@@ -19,22 +19,6 @@ class MainActivity : AppCompatActivity(), StoryEventListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        etToken.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-
-            override fun beforeTextChanged(
-                s: CharSequence?,
-                start: Int,
-                count: Int,
-                after: Int
-            ) {
-            }
-        })
-
         swipeToRefresh.setOnRefreshListener {
             applyStories()
         }
